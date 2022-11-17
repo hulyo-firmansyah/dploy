f03e3f311aa27b5966a5b16e513b11c551266678
HUL.PC-ATAS.WIN <axusxzbitcomp@gmail.com>
2022-11-17 16:14:57 +0700

    bikin testcase

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,10 +1,69 @@
 import { test, expect } from "@playwright/test"
 
-test("Should navigate to Home", async ({ page }) => {
-	await page.goto("./")
+test("Should Have Title", async ({ page }) => {
+	await page.goto("/")
 
-	await expect(page).toHaveTitle(/Create Next App/gi)
+	await expect(page).toHaveTitle(/Landing/g)
+})
 
-	const textBanner = await page.locator("#welcome").textContent()
-	expect(textBanner).toBe("Welcome to Next.js!")
+test("Should show banner text", async ({ page }) => {
+	await page.goto("/")
+
+	const innerText = page.locator("[data-testid=banner-text]")
+	await expect(innerText).toContainText(/(Deploy|DPloy)/g)
+})
+
+test("Check server running mesasge", async ({ page }) => {
+	await page.goto("/")
+
+	await expect(page.locator("text=Server Running on")).toContainText(
+		/production/g
+	)
+})
+
+test("Click get started button redirect properly", async ({ page }) => {
+	await page.goto("/")
+
+	await page.locator("text=Get Started").click()
+
+	expect(page.url()).toContain("/docs")
+
+	await page.locator("data-testid=logo-link").click()
+
+	expect(page.url()).toContain("/")
+})
+
+test("Click find out button redirect properly", async ({ page }) => {
+	await page.goto("/")
+
+	await page.locator("text=Find Out").click()
+
+	expect(page.url()).toContain("/docs")
+
+	await page.locator("data-testid=logo-link").click()
+
+	expect(page.url()).toContain("/")
+})
+
+test("Click Docs button", async ({ page }) => {
+	await page.goto("/")
+
+	await page.locator("text=Docs").click()
+
+	expect(page.url()).toContain("/docs")
+
+	await page.locator("data-testid=logo-link").click()
+
+	expect(page.url()).toContain("/")
+})
+
+test("Click Blog button", async ({ page }) => {
+	await page.goto("/")
+
+	await page.locator("text=Blog").click()
+
+	expect(page.url()).toContain("/blog")
+
+	const text = await page.locator("body").innerText()
+	expect(text).toMatch(/404/gim)
 })
