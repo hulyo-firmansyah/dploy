98bd22de00357e3d24cc0cf3a88159865128f091
HUL.PC-ATAS.WIN <axusxzbitcomp@gmail.com>
2022-11-19 13:48:40 +0700

    halaman login

---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router"
+import { Fragment } from "react"
+
+const Login = () => {
+	/**
+	 * Router
+	 */
+	const router = useRouter()
+
+	/**
+	 * Effects
+	 */
+
+	return <Fragment>Okez</Fragment>
+}
+
+export default Login
