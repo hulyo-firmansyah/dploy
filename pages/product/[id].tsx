c423595cd84303ee77626f7f194138d038889bc6
HUL.PC-ATAS.WIN <axusxzbitcomp@gmail.com>
2022-11-19 13:31:38 +0700

    update product title on product detail

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -3,6 +3,8 @@ import { Product } from "../../types/types"
 import axios from "axios"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { Fragment } from "react"
+import Head from "next/head"
 
 export const getStaticPaths: GetStaticPaths = async () => {
 	// When this is true (in preview environments) don't
@@ -100,63 +102,68 @@ const ProductDetail = (
 	}
 
 	return (
-		<div className="container mt-14 pb-20">
-			<div className="gap-14 rounded-md bg-slate-900 p-5 shadow-md md:p-10 lg:flex lg:p-20">
-				<div className="image-container relative h-full w-full flex-shrink-0 lg:h-96 lg:w-96">
-					<Image
-						alt={props.product?.title}
-						src={props.product?.image ?? ""}
-						layout="responsive"
-						width={500}
-						height={500}
-						objectFit={"cover"}
-						className="rounded-xl shadow-lg"
-					/>
-				</div>
+		<Fragment>
+			<Head>
+				<title>{props.product?.title} · DPloy</title>
+			</Head>
+			<div className="container mt-14 pb-20">
+				<div className="gap-14 rounded-md bg-slate-900 p-5 shadow-md md:p-10 lg:flex lg:p-20">
+					<div className="image-container relative h-full w-full flex-shrink-0 lg:h-96 lg:w-96">
+						<Image
+							alt={props.product?.title}
+							src={props.product?.image ?? ""}
+							layout="responsive"
+							width={500}
+							height={500}
+							objectFit={"cover"}
+							className="rounded-xl shadow-lg"
+						/>
+					</div>
 
-				<div className="mt-5">
-					<div className="text-sm font-semibold text-slate-400 md:text-base">
-						{props.product?.category}
-					</div>
-					<div className="title mt-1 text-lg font-semibold md:text-2xl lg:font-bold lg:tracking-tight">
-						{props.product?.title}
-					</div>
-					<div className="mt-3 flex items-center justify-between divide-slate-700 md:justify-start md:gap-5">
-						<div className="price font-bold text-slate-300 md:text-lg">
-							<span className="text-cyan-400">$</span>
-							{props.product?.price}
+					<div className="mt-5">
+						<div className="text-sm font-semibold text-slate-400 md:text-base">
+							{props.product?.category}
 						</div>
-						<div className="border border-y-0 border-l-0 border-white">
-							<div className="opacity-0">|</div>
+						<div className="title mt-1 text-lg font-semibold md:text-2xl lg:font-bold lg:tracking-tight">
+							{props.product?.title}
 						</div>
-						<div className="flex items-center gap-1 text-slate-400">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								fill="currentColor"
-								className="inline-block h-4 w-4 text-yellow-500"
-							>
-								<path
-									fillRule="evenodd"
-									d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-									clipRule="evenodd"
-								/>
-							</svg>
-							<span className="rating font-bold text-yellow-500">
-								{props.product?.rating.rate}
-							</span>
-							<span> / </span>
-							<span className="rating-count">
-								{props.product?.rating.count}
-							</span>
+						<div className="mt-3 flex items-center justify-between divide-slate-700 md:justify-start md:gap-5">
+							<div className="price font-bold text-slate-300 md:text-lg">
+								<span className="text-cyan-400">$</span>
+								{props.product?.price}
+							</div>
+							<div className="border border-y-0 border-l-0 border-white">
+								<div className="opacity-0">|</div>
+							</div>
+							<div className="flex items-center gap-1 text-slate-400">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									className="inline-block h-4 w-4 text-yellow-500"
+								>
+									<path
+										fillRule="evenodd"
+										d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+										clipRule="evenodd"
+									/>
+								</svg>
+								<span className="rating font-bold text-yellow-500">
+									{props.product?.rating.rate}
+								</span>
+								<span> / </span>
+								<span className="rating-count">
+									{props.product?.rating.count}
+								</span>
+							</div>
 						</div>
+						<p className="description mt-7 text-sm text-slate-400 md:text-base lg:mt-10">
+							{props.product?.description}
+						</p>
 					</div>
-					<p className="description mt-7 text-sm text-slate-400 md:text-base lg:mt-10">
-						{props.product?.description}
-					</p>
 				</div>
 			</div>
-		</div>
+		</Fragment>
 	)
 }
 
